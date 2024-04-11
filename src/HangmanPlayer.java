27e0f67976fcdea902d321a669bba5a7fc565c46
VFrogale <124105497+VFrogale@users.noreply.github.com>
2024-04-11 17:49:41 -0400

    Didn't add anything

--- src/HangmanPlayer.java
+++ src/HangmanPlayer.java
@@ -18,36 +18,44 @@ import java.io.FileNotFoundException;
 
 public class HangmanPlayer {
 
-    // initialize HangmanPlayer with a file of English words
-    // Pre-processing the word file that contains a list of English words in each line
-    // Array of lists 
-    public HangmanPlayer(String wordFile) throws FileNotFoundException {
-        final Scanner wordFileInput = new Scanner(new File(wordFile));
-    }
+  // initialize HangmanPlayer with a file of English words
+  // Pre-processing the word file that contains a list of English words in each
+  // line
+  // Array of lists
+  public HangmanPlayer(String wordFile) throws FileNotFoundException {
 
-    // based on the current (partial or intitially blank) word
-    //    guess a letter
-    // currentWord: current word, currenWord.length has the length of the hidden word
-    // isNewWord: indicates a new hidden word
-    // returns the guessed letter
-    // assume all letters are in lower case
-    public char guess(String currentWord, boolean isNewWord) {
-        char guess = ' ';
+    final Scanner wordFileInput = new Scanner(new File(wordFile));
 
-        return guess;
-    }
-
-    // feedback on the guessed letter
-    // isCorrectGuess: true if the guessed letter is one of the letters in the hidden word
-    // currentWord: partially filled or blank word
-    //   
-    // Case       isCorrectGuess      currentWord   
-    // a.         true                partial word with the guessed letter
-    //                                   or the whole word if the guessed letter was the
-    //                                   last letter needed
-    // b.         false               partial word without the guessed letter
-    public void feedback(boolean isCorrectGuess, String currentWord) {
+    while (wordFileInput.hasNextLine()) {
 
     }
+  }
+
+  // based on the current (partial or intitially blank) word
+  // guess a letter
+  // currentWord: current word, currenWord.length has the length of the hidden
+  // word
+  // isNewWord: indicates a new hidden word
+  // returns the guessed letter
+  // assume all letters are in lower case
+  public char guess(String currentWord, boolean isNewWord) {
+    char guess = ' ';
+
+    return guess;
+  }
+
+  // feedback on the guessed letter
+  // isCorrectGuess: true if the guessed letter is one of the letters in the
+  // hidden word
+  // currentWord: partially filled or blank word
+  //
+  // Case isCorrectGuess currentWord
+  // a. true partial word with the guessed letter
+  // or the whole word if the guessed letter was the
+  // last letter needed
+  // b. false partial word without the guessed letter
+  public void feedback(boolean isCorrectGuess, String currentWord) {
+
+  }
 
 }
